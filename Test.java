984197204864543e73ec0347012d7f2f1b3f7483
somaddddd064-eng <somaddddd064@gmail.com>
2025-10-02 15:24:35 +0700

    pesan 2

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini barisan kode pertama");
         System.out.println("Iya ");
-        
+        System.out.println("moleh jam piro");
     }
 }
