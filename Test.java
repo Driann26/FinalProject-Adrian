f8287dd6d7d8b28a02957f6593102ee1c25cb17a
somaddddd064-eng <somaddddd064@gmail.com>
2025-10-02 15:21:05 +0700

    pesan

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini barisan kode pertama");
+        System.out.println("Iya ");
+        
     }
 }
